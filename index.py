faa03f4e150640da55303345531602d4fd4c3ee9
Darwin <115344849+darwin242209@users.noreply.github.com>
2023-04-28 22:28:08 +0800

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryH
 from movies_scraper import search_movies, get_movie
 
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("6224861474:AAGnY1aqA4He3RES4rQN4jRlcfg3GYm0svg")
 URL = os.getenv("URL")
 bot = Bot(TOKEN)
 
